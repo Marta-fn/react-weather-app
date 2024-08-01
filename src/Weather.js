e146eb30f78b4b0e2d19b75f806815b45551e15c
Marta Nogueira <martoocas.fn@gmail.com>
2024-08-01 13:17:55 +0100

    Added basic CSS

--- src/Weather.js
+++ src/Weather.js
@@ -5,27 +5,51 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input type="search" placeholder="Enter a city..." />
-        <input type="submit" value="Search" className="btn btn-primary" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              autoFocus="on"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
       </form>
       <h1>Viseu</h1>
       <ul>
         <li>Wednesday 07:00</li>
         <li>Sunny</li>
       </ul>
-      <img
-        src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-        alt="Sunny"
-      />
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <p>22ºC</p>
+          <div className="d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
+            />
+            <span className="temperature">22</span>
+            <span className="unit">ºC</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 0%</li>
-            <li>Humidity: 73%</li>
-            <li>Wind: 8Km/h</li>
+            <li>
+              <strong>Precipitation:</strong> 0%
+            </li>
+            <li>
+              <strong>Humidity:</strong> 73%
+            </li>
+            <li>
+              <strong>Wind:</strong> 8Km/h
+            </li>
           </ul>
         </div>
       </div>
